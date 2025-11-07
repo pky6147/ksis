f0debd949254b3ed47837de14e7012cf3623cef4
ksm4949 <124114309+ksm4949@users.noreply.github.com>
2025-11-07 13:38:26 +0900

    셀렉트컴포넌트, 유저등록페이지

--- front/src/component/CustomTextField.tsx
+++ front/src/component/CustomTextField.tsx
@@ -4,11 +4,12 @@ import {Box, TextField} from '@mui/material'
 import {type TextField_Type} from '../Types/Components'
 
 export default function CustomTextField(props: TextField_Type) {
-    const {value, label, variant, inputWidth, disabled, placeholder, readOnly, type, onChange, startAdornment, endAdornment} = props
+    const {value, label, variant, border, inputWidth, disabled, placeholder, readOnly, type, 
+        onChange, startAdornment, endAdornment} = props
     return (
         <Box sx={{ display: 'flex', alignItems: 'center', gap: 1, minWidth: '320px'}}>
             <TextField 
-                sx={{backgroundColor: 'white', borderRadius: 1, border: '1px solid', width: inputWidth || '246px', minWidth: '246px' }}
+                sx={{backgroundColor: 'white', borderRadius: 1, border: border || '1px solid', width: inputWidth || '246px', minWidth: '246px' }}
                 size= "small"
                 value={value || ''}
                 label={label || ''}
--- front/src/page/01_UserManagement/RegPage.tsx
+++ front/src/page/01_UserManagement/RegPage.tsx
@@ -1,7 +1,11 @@
 import {useState} from 'react'
-import {Box, Typography, Button} from '@mui/material'
+import {Box, Typography, InputAdornment, type SelectChangeEvent} from '@mui/material'
+import CustomButton from '../../component/CustomButton';
+import CustomTextField from '../../component/CustomTextField';
+import CustomIconButton from '../../component/CustomIconButton';
+import CustomSelect from '../../component/CustomSelect';
 
-import { type UserTableRows } from '../../Types/TableHeaders/UserManageHeader'
+// import { type UserTableRows } from '../../Types/TableHeaders/UserManageHeader'
 
 interface RegPageProps {
     handleDone: () => void;
@@ -10,20 +14,252 @@ interface RegPageProps {
 
 export default function RegPage(props: RegPageProps) {
     const {handleDone, handleCancle} = props
+    const [isVisible, setIsVisible] = useState(false)
+    const [isPasswordMismatch, setIsPasswordMismatch] = useState(false);
+    const [newData, setNewData] = useState({
+        loginId: '',
+        password: '',
+        passwordConfirm: '',
+        name: '',
+        dept: '',
+        rank: '',
+        state: '',
+    })
+    const stateList = [
+        { value: 'Y', name: '승인' },
+        { value: 'N', name: '대기중' },
+    ];
 
-    const [target, setTarget] = useState<UserTableRows>()
-
-    const handleRegist = () => {
-        // 여기서 수정작업 
-        console.log("Dialog 안 Edit", target)
-        handleDone()
+    const handleShowPassword = () => {
+        setIsVisible(!isVisible);
     }
 
+    const handleInputChange = (key: keyof typeof newData, value: string) => {
+        setNewData((prev) => {
+            const updated = { ...prev, [key]: value };
+            
+            if (key === 'passwordConfirm' || key === 'password') {
+                const mismatch = updated.password !== updated.passwordConfirm;
+                setIsPasswordMismatch(mismatch);
+            }
+
+            return updated;
+        });
+    }
+
+    const handleSelectChange = (key: keyof typeof newData) => 
+    (event: SelectChangeEvent<string | number>) => {
+      setNewData((prev) => ({ ...prev, [key]: event.target.value }));
+    };
+
+    const handleRegist = () => {
+        const password = newData.password;
+        const passwordConfirm = newData.passwordConfirm;
+
+        // 한글 포함 여부 검사
+        const hasKorean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(password);
+        const hasKoreanC = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(passwordConfirm);
+
+        if (hasKorean || hasKoreanC) {
+            console.log('hasKorean', hasKorean)
+            console.log('hasKoreanC', hasKoreanC)
+            alert('비밀번호에 한글은 포함될 수 없습니다.');
+            return;
+        }
+
+        // handleDone()
+    }
+
+
     return (
-        <Box>
-            <Typography>{target? target.name : 'none'}</Typography>
-            <Button onClick={handleRegist} >완료</Button>
-            <Button onClick={handleCancle} >취소</Button>
+        <Box sx={{
+            width: '50vw',
+            height: '65vh',
+            backgroundColor: 'white',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between'
+        }}>
+            <Box sx={{borderBottom: '1px solid black'}}>
+                <Typography sx={{fontSize: 64, fontWeight: 'bold', marginLeft: '20px'}}>사용자 등록</Typography>
+            </Box>
+            <Box sx={{
+                border: '2px solid #abababff',
+                marginLeft: '20px',
+                marginRight: '20px',
+                paddingTop: 1,
+                paddingBottom: 1
+            }}>
+                {/* ID */}
+                <Box sx={{display: 'flex', justifyContent: 'space-around', gap: 2, padding: 1}}>
+                    <Box sx={{display: 'flex', justifyContent:'center', alignItems: 'center', border: '3px solid black', borderRadius: 1 ,width: '200px'}}>
+                        <Typography>아이디</Typography>
+                        <Typography sx={{color: 'red'}}>*</Typography>
+                    </Box>
+                    <Box sx={{display: 'flex', flexDirection: 'column', gap: 1}}>
+                        <CustomTextField 
+                          variant="outlined"
+                          value={newData.loginId}
+                          border="3px solid #757575"
+                          inputWidth="300px"
+                          disabled={false}
+                          readOnly={false}
+                          placeholder="아이디"
+                          type="text"
+                          onChange={(e) => handleInputChange('loginId', e.target.value)}
+                        />
+                        <Box sx={{ backgroundColor: '#c5c4c7', border: '3px solid #757575', borderRadius:1, width: '300px'}}>
+                            <Typography>∴ 여기는 중복 여부 표시</Typography>
+                            <Typography>∴ 여기는 아이디 설정 조건</Typography>
+                        </Box>
+                    </Box>
+                </Box>
+                {/* 비밀번호 */}
+                <Box sx={{display: 'flex', justifyContent: 'space-around', gap: 2, padding: 1}}>
+                    <Box sx={{display: 'flex', justifyContent:'center', alignItems: 'center', border: '3px solid black', borderRadius: 1 ,width: '200px'}}>
+                        <Typography>비밀번호</Typography>
+                        <Typography sx={{color: 'red'}}>*</Typography>
+                    </Box>
+                    <Box sx={{display: 'flex', flexDirection: 'column', gap: 1}}>
+                        <CustomTextField 
+                            variant="outlined"
+                            value={newData.password}
+                            border="3px solid #757575"
+                            inputWidth="300px"
+                            disabled={false}
+                            readOnly={false}
+                            placeholder="비밀번호"
+                            type={isVisible? 'text' : "password"}
+                            onChange={(e) => handleInputChange('password', e.target.value)}
+                            endAdornment={
+                              <InputAdornment position="end">
+                                { isVisible?
+                                    (<CustomIconButton icon="invisible" width='20px' height='20px' color="gray" onClick={handleShowPassword} />) :
+                                    (<CustomIconButton icon="visible"   width='20px' height='20px' color="gray" onClick={handleShowPassword} />) 
+                                }
+                              </InputAdornment>
+                            }
+                        />
+                        <Box sx={{ backgroundColor: '#c5c4c7', border: '3px solid #757575', borderRadius:1, width: '300px'}}>
+                            <Typography>∴ 비밀번호 사용 가능 확인 메시지</Typography>
+                            <Typography>∴ 여기는 비밀번호 설정 조건</Typography>
+                        </Box>
+                    </Box>
+                </Box>
+                {/* 비밀번호 확인 */}
+                <Box sx={{display: 'flex', justifyContent: 'space-around', gap: 2, padding: 1}}>
+                    <Box sx={{display: 'flex', justifyContent:'center', alignItems: 'center', border: '3px solid black', borderRadius: 1 ,width: '200px'}}>
+                        <Typography>비밀번호 확인</Typography>
+                    </Box>
+                    <Box sx={{display: 'flex', flexDirection: 'column', gap: 1}}>
+                        <CustomTextField 
+                            variant="outlined"
+                            value={newData.passwordConfirm}
+                            border="3px solid #757575"
+                            inputWidth="300px"
+                            disabled={false}
+                            readOnly={false}
+                            placeholder="비밀번호 확인"
+                            type={isVisible? 'text' : "password"}
+                            onChange={(e) => handleInputChange('passwordConfirm', e.target.value)}
+                            endAdornment={
+                              <InputAdornment position="end">
+                                { isVisible?
+                                    (<CustomIconButton icon="invisible" width='20px' height='20px' color="gray" onClick={handleShowPassword} />) :
+                                    (<CustomIconButton icon="visible"   width='20px' height='20px' color="gray" onClick={handleShowPassword} />) 
+                                }
+                              </InputAdornment>
+                            }
+                        />
+                        <Box sx={{ backgroundColor: '#c5c4c7', border: '3px solid #757575', borderRadius:1, width: '300px', 
+                            overflow: 'hidden', // 높이 줄이기 위해 꼭 필요
+                            height: isPasswordMismatch  ? 'auto' : 0,
+                            opacity: isPasswordMismatch  ? 1 : 0,
+                            transition: 'all 0.3s ease', // 부드럽게 등장/사라짐
+                        }}>
+                            <Typography sx={{color: 'red'}}>∴ 입력한 비밀번호가 다릅니다.</Typography>
+                            <Typography sx={{color: 'red', whiteSpace: 'pre'}}>{'     '}비밀번호를 확인해주세요.</Typography>
+                        </Box>
+                    </Box>
+                </Box>
+                {/* 이름 */}
+                <Box sx={{display: 'flex', justifyContent: 'space-around', gap: 2, padding: 1}}>
+                    <Box sx={{display: 'flex', justifyContent:'center', alignItems: 'center', border: '3px solid black', borderRadius: 1 ,width: '200px'}}>
+                        <Typography>이름</Typography>
+                        <Typography sx={{color: 'red'}}>*</Typography>
+                    </Box>
+                    <Box>
+                        <CustomTextField 
+                          variant="outlined"
+                          value={newData.name}
+                          border="3px solid #757575"
+                          inputWidth="300px"
+                          disabled={false}
+                          readOnly={false}
+                          placeholder="이름"
+                          type="text"
+                          onChange={(e) => handleInputChange('name', e.target.value)}
+                        />
+                    </Box>
+                </Box>
+                {/* 부서 */}
+                <Box sx={{display: 'flex', justifyContent: 'space-around', gap: 2, padding: 1}}>
+                    <Box sx={{display: 'flex', justifyContent:'center', alignItems: 'center', border: '3px solid black', borderRadius: 1 ,width: '200px'}}>
+                        <Typography>부서</Typography>
+                    </Box>
+                    <Box>
+                        <CustomTextField 
+                          variant="outlined"
+                          value={newData.dept}
+                          border="3px solid #757575"
+                          inputWidth="300px"
+                          disabled={false}
+                          readOnly={false}
+                          placeholder="부서"
+                          type="text"
+                          onChange={(e) => handleInputChange('dept', e.target.value)}
+                        />
+                    </Box>
+                </Box>
+                {/* 직위 */}
+                <Box sx={{display: 'flex', justifyContent: 'space-around', gap: 2, padding: 1}}>
+                    <Box sx={{display: 'flex', justifyContent:'center', alignItems: 'center', border: '3px solid black', borderRadius: 1 ,width: '200px'}}>
+                        <Typography>직위</Typography>
+                    </Box>
+                    <Box>
+                        <CustomTextField 
+                          variant="outlined"
+                          value={newData.rank}
+                          border="3px solid #757575"
+                          inputWidth="300px"
+                          disabled={false}
+                          readOnly={false}
+                          placeholder="직위"
+                          type="text"
+                          onChange={(e) => handleInputChange('rank', e.target.value)}
+                        />
+                    </Box>
+                </Box>
+                {/* 승인상태 */}
+                <Box sx={{display: 'flex', justifyContent: 'space-around', gap: 2, padding: 1}}>
+                    <Box sx={{display: 'flex', justifyContent:'center', alignItems: 'center', border: '3px solid black', borderRadius: 1 ,width: '200px'}}>
+                        <Typography>승인상태</Typography>
+                        <Typography sx={{color: 'red'}}>*</Typography>
+                    </Box>
+                    <Box sx={{marginRight: '15px'}}>
+                        <CustomSelect
+                          value={newData.state}
+                          listItem={stateList}
+                          onChange={handleSelectChange('state')}
+                          border="3px solid #757575"
+                        />
+                    </Box>
+                </Box>
+            </Box>
+            <Box sx={{display: 'flex', justifyContent: 'center', gap:2, marginBottom: 2}}>
+                <CustomButton text="등록" onClick={handleRegist} />
+                <CustomButton text="닫기" onClick={handleCancle} />
+            </Box>
         </Box>
     )
 }
